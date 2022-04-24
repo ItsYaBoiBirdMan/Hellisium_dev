c5093e58d03e77ec11276209d2a5039805ca0616
ItsYaBoiBirdMan <94912025+ItsYaBoiBirdMan@users.noreply.github.com>
2022-04-24 23:32:05 +0100

    Please work

--- public/javascripts/gamelib.js
+++ public/javascripts/gamelib.js
@@ -11,19 +11,20 @@ async function setup() {
     var canvas = createCanvas(width, height);
     id = 3
     canvas.parent('game');
-    cards = await getCard(id);
+    cards = await getCards();
 }
 async function draw() {
     background(220);
     let x=50;
-    if (cards) 
-    text(cards.card_name, x, 100, 70, 50);
-    text('ATK', x - 7, 135, 70, 50)
-    text('HP', x + 27, 135, 70, 50)
-    text(cards.card_hp, x + 30, 150, 70,50);
-    text(cards.card_atk, x, 150, 70, 50)
-    x+=100
-
+    if (cards)
+    for(let card of cards){
+        text(card.card_name, x, 100, 70, 50);
+        text('ATK', x - 7, 135, 70, 50)
+        text('HP', x + 27, 135, 70, 50)
+        text(card.card_hp, x + 30, 150, 70,50);
+        text(card.card_atk, x, 150, 70, 50)
+        x += 100
+    }
 }
 
 function changeCradId(newId){
